e546c1a003199c35c17c5bd63f9171ee1a9de6f5
Alex <muchenealex24@gmail.com>
2024-11-04 18:44:22 +0300

    Final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { app } from "../fierbase";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 const Home = () => {
-  return <img className=" w-full h-screen " src="/Alin.jpg" alt="" />;
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    const storage = getStorage(app);
+    const fileRef = ref(storage, "gs://mern-stock.appspot.com/Alin.jpg"); // Replace with your file path
+
+    getDownloadURL(fileRef)
+      .then((url) => {
+        setImageUrl(url);
+      })
+      .catch((error) => {
+        console.error("Error getting download URL:", error);
+      });
+  }, []);
+  return <img className=" w-full h-screen " src={imageUrl} alt="" />;
 };
 
 export default Home;
